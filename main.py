5caac6d6352310999802fc0491f9ae39b3a5a02c
vadim <dobrodeiateli@gmail.com>
2022-04-30 23:01:00 +0300

    tg_bot_1

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot('5120447001:AAFga0SiU3AGkdR0-8WeEK6NY_5bNEMzjaU')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(messge):
+    mess = f'privet, <b> {messge.from_user.first_name} <u>{messge.from_user.last_name}</u></b>'
+    bot.send_message(messge.chat.id, mess, parse_mode='html')
 
 
 
-print("Hello Git")
-x = 7
-print(x)
+bot.polling(none_stop=True)
