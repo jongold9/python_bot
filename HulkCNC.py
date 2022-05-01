96902eda80b70b3875fe975a145d95caae41a353
vadim <dobrodeiateli@gmail.com>
2022-05-01 11:57:32 +0300

    Hulk_base

--- HulkCNC.py
+++ HulkCNC.py
@@ -4,6 +4,24 @@ from telebot import types
 bot = telebot.TeleBot('5120447001:AAFga0SiU3AGkdR0-8WeEK6NY_5bNEMzjaU')
 
 @bot.message_handler(commands=['start'])
-def start(messge):
-    mess = f'privet, <b> {messge.from_user.first_name} <u>{messge.from_user.last_name}</u></b>'
-    bot.send_message(messge.chat.id, mess, parse_mode='html')
+def website(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    start = types.KeyboardButton('/start')
+    help = types.KeyboardButton('help')
+    website = types.KeyboardButton('website')
+
+    markup.add(start, help, website)
+
+    bot.send_message(message.chat.id, 'Привет, в меню снизу ты можешь выбрать интересующую тебя тему', reply_markup=markup)
+
+@bot.message_handler()
+def get_user_text(message):
+    if message.text == "help":
+        bot.send_message(message.chat.id, "Hello, I help you", parse_mode='html')
+    elif message.text == "website":
+        bot.send_message(message.chat.id, "https://google.com", parse_mode='html')
+    else:
+        bot.send_message(message.chat.id, "I don't understand you", parse_mode='html')
+
+
+bot.polling(none_stop=True)
